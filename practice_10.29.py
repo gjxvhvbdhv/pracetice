8b59f2bcfbd403fc7643a4a2032f8d7ecafff2ff
gjxvhvbdhv <73735994+gjxvhvbdhv@users.noreply.github.com>
2020-11-09 17:05:20 +0800

    Add files via upload

---
+++ practice_10.29.py
@@ -0,0 +1,48 @@
+# 2020.10.29 学习
+""" name = "ada"
+last = "lovelace"
+full = f"{name}{last} "
+print(full)
+print(full.rstrip())
+print("python\nc\njavascript")
+ """
+
+""" str = "     this is string example....wow!!!     ";
+print str.rstrip();
+str = "88888888this is string example....wow!!!8888888";
+print str.rstrip('8'); """
+
+""" name = "python "
+name """
+
+""" universe = 1000_0000_00000
+print(universe) """
+
+""" a = 0.01+0.05
+print(a) """
+
+""" a = 8*1
+b = 0.1+7.9
+c = 2+6
+d = 2.0+6.0
+print(a)
+print(b)
+print(c)
+print(d) """
+
+# import this
+
+bicycles = ['trek', 'connondale', 'redline', 'giant']
+print(bicycles[0].title())
+# massage = f"my first bicycle was a {bicycles[0].title()}"
+print(f"my first bicycle was a {bicycles[0].title()}")
+
+motorcycles = ['yahama', 'honda', 'suzuki']
+print(motorcycles)
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = ['yahama', 'honda', 'suzuki']
+print(motorcycles)
+motorcycles.insert(1,'ducati')
+print(motorcycles)
